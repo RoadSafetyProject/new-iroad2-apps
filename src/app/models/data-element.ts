5005524fcbf527d8f29ec41dc78e92a0bbd4821d
Vincent P. Minde <vincentminde@gmail.com>
2016-11-05 15:42:14 +0300

    added dataelement model

---
+++ src/app/models/data-element.ts
@@ -0,0 +1,38 @@
+export class IDataElement {
+  id:string;
+  name:string;
+  shortName:string;
+  code:string;
+  description:string;
+  valueType:string;
+  domainType:string;
+  formName:string;
+  aggregationType:string;
+  optionset:string;
+}
+export class DataElement implements IDataElement{
+  id:string;
+  name:string;
+  shortName:string;
+  code:string;
+  description:string;
+  valueType:string;
+  domainType:string;
+  formName:string;
+  aggregationType:string;
+  optionset:string;
+
+  constructor(dataElement:IDataElement){
+    this.id = dataElement.id;
+    this.name = dataElement.name;
+    this.shortName = dataElement.shortName;
+    this.code = dataElement.code;
+    this.description = dataElement.description;
+    this.valueType = dataElement.valueType;
+    this.domainType = dataElement.domainType;
+    this.formName = dataElement.formName;
+    this.aggregationType = dataElement.aggregationType;
+    this.optionset = dataElement.optionset;
+  }
+
+}

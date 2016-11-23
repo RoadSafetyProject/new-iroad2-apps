ec6d7ecb319c65a1354eb6e791d6bb6ea240d66b
Vincent P. Minde <vincentminde@gmail.com>
2016-11-23 11:22:31 +0300

    Added some vehicle issues

--- src/app/apps/vehicle-capture/add-vehicle/add-vehicle.component.ts
+++ src/app/apps/vehicle-capture/add-vehicle/add-vehicle.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {VehicleService} from "../../../services/vehicle.service";
+import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-add-vehicle',
   templateUrl: './add-vehicle.component.html',
-  styleUrls: ['./add-vehicle.component.css']
+  styleUrls: ['./add-vehicle.component.css'],
+  providers:[VehicleService]
 })
 export class AddVehicleComponent implements OnInit {
 
-  constructor() { }
+  private vehicleProgramStage;
+  private relationDataElementValueObject: any = {};
+  id;
+
+  constructor(private vehicleService:VehicleService,private route: ActivatedRoute,private router: Router) {
+    this.route.params.forEach((params: Params) => {
+      this.id = params['id'];
+    });
+  }
 
   ngOnInit() {
+    this.vehicleService.getProgram().then((vehicleProgramStage : any) =>{
+      this.vehicleProgramStage = vehicleProgramStage;
+      let relationDataElement = "Program_Vehicle";
+      vehicleProgramStage.programStageDataElements.forEach((programStageDataElement :any)=>{
+        if(programStageDataElement.dataElement.name.toLowerCase() == relationDataElement.toLowerCase()){
+          this.relationDataElementValueObject[programStageDataElement.dataElement.id] = this.id;
+        }
+      })
+    })
   }
 
 }

db463cf08014e94a436ceff5253b098e0ce2606e
Joseph Chingalo <profschingalo@gmail.com>
2016-12-02 15:07:45 +0300

    fixing displya for accidents associate by vehicle

--- src/app/apps/vehicle-capture/view-vehicle-accidents/view-vehicle-accidents.component.ts
+++ src/app/apps/vehicle-capture/view-vehicle-accidents/view-vehicle-accidents.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {AccidentService} from "../../../services/accident.service";
 import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
+import {AccidentVehicleService} from "../../../services/accident-vehicle.service";
 
 @Component({
   selector: 'app-view-vehicle-accidents',
   templateUrl: './view-vehicle-accidents.component.html',
   styleUrls: ['./view-vehicle-accidents.component.css'],
-  providers:[AccidentService]
+  providers:[AccidentService,AccidentVehicleService]
 })
 export class ViewVehicleAccidentsComponent implements OnInit {
 
@@ -14,37 +15,36 @@ export class ViewVehicleAccidentsComponent implements OnInit {
   private accidentProgram;
   private accidents;
   id;
-  constructor(private accidentService:AccidentService,private route: ActivatedRoute,private router: Router) {
+  constructor(private AccidentService:AccidentService,private AccidentVehicleService: AccidentVehicleService,private route: ActivatedRoute,private router: Router) {
     this.route.params.forEach((params: Params) => {
       this.id = params['id'];
     });
-    this.initiateMenuActions();
   }
 
   ngOnInit() {
-    this.accidentService.getProgram().then((accidentProgram) =>{
-      this.accidentProgram = accidentProgram;
-      this.accidentService.getEventsByVehicle(this.id).then(accidents =>{
-        this.accidents = accidents;
-      })
-    })
+    this.AccidentService.getProgram().then(accidentProgramStage=>{
+      this.accidentProgram = accidentProgramStage;
+      this.AccidentVehicleService.getProgram().then((programStage : any)=>{
+        this.AccidentVehicleService.getAccidentIdsByVehicle(this.id).then(accidentIds=>{
+          this.AccidentService.getAccidentsByIds(accidentIds).then(events=>{
+            this.accidents = events;
+          },error=>{
+            //fail to get events
+          });
+        },error=>{
+          //fail to get accident ids
+        });
+      },error=>{
+        //fail to load accident vehicles
+      });
+    },error=>{
+      //fail to load accident program stage
+    });
   }
 
   search(searchText) {
-    console.log(searchText);
-    if(searchText == ""){
-      this.ngOnInit()
-    }else{
-      this.accidentService.searchEvents(searchText).then(accidents =>{
-        this.accidents = accidents;
-      })
-    }
-  }
-  initiateMenuActions(){
-    this.menuActions =[
-      {
-        title : "View Business Licence Details",
-        url : "sumatra/business-history/:id"
-      }];
+    this.AccidentService.searchEvents(searchText).then(drivers =>{
+      this.accidents = drivers;
+    })
   }
 }

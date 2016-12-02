474e3572a126f5a1c877d1b17003752022276f60
Joseph Chingalo <profschingalo@gmail.com>
2016-12-02 15:23:08 +0300

    implement update vehicle detail

--- src/app/apps/vehicle-capture/update-vehicle/update-vehicle.component.ts
+++ src/app/apps/vehicle-capture/update-vehicle/update-vehicle.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {VehicleService} from "../../../services/vehicle.service";
+import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-update-vehicle',
   templateUrl: './update-vehicle.component.html',
-  styleUrls: ['./update-vehicle.component.css']
+  styleUrls: ['./update-vehicle.component.css'],
+  providers:[VehicleService]
 })
 export class UpdateVehicleComponent implements OnInit {
 
-  constructor() { }
+  vehicle;
+  id;
+  vehicleProgramStage;
+
+  constructor(private vehicleService:VehicleService,private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      this.id = params['id'];
+    });
+    this.vehicleService.getProgram().then((vehicleProgram) =>{
+      this.vehicleProgramStage = vehicleProgram;
+      this.vehicleService.get(this.id).then((vehicle : any) =>{
+        this.vehicle = vehicle;
+      })
+    })
   }
 
 }

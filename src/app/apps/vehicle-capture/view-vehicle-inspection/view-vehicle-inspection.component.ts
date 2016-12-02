a1897fe9148129211140127b95d0629d41a0d618
Vincent P. Minde <vincentminde@gmail.com>
2016-12-02 16:01:03 +0300

    Fixed some issues

--- src/app/apps/vehicle-capture/view-vehicle-inspection/view-vehicle-inspection.component.ts
+++ src/app/apps/vehicle-capture/view-vehicle-inspection/view-vehicle-inspection.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import {VehicleInspectionService} from "../../../services/vehicle-inspection.service";
+import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-view-vehicle-inspection',
   templateUrl: './view-vehicle-inspection.component.html',
-  styleUrls: ['./view-vehicle-inspection.component.css']
+  styleUrls: ['./view-vehicle-inspection.component.css'],
+  providers:[VehicleInspectionService]
 })
 export class ViewVehicleInspectionComponent implements OnInit {
 
-  constructor() { }
+  private menuActions : any;
+  private vehicleInspectionProgram;
+  private vehicleInspections;
+  id;
+  constructor(private vehicleInspectionService:VehicleInspectionService,private route: ActivatedRoute) {
+    this.route.params.forEach((params: Params) => {
+      this.id = params['id'];
+    });
+  }
 
   ngOnInit() {
+    this.initiateMenuActions();
+    this.vehicleInspectionService.getProgram().then((vehicleInspectionProgram) =>{
+      this.vehicleInspectionProgram = vehicleInspectionProgram;
+      this.vehicleInspectionService.getEvents().then(vehicleInspections =>{
+        this.vehicleInspections = vehicleInspections;
+      })
+    })
+  }
+  search(searchText) {
+    console.log(searchText);
+    this.vehicleInspectionService.searchEvents(searchText).then(vehicleInspections =>{
+      this.vehicleInspections = vehicleInspections;
+    })
+  }
+  pageChange(page) {
+    this.vehicleInspectionService.getEvents({page:page}).then(vehicleInspections =>{
+      this.vehicleInspections = vehicleInspections;
+    })
+  }
+
+  initiateMenuActions(){
+    this.menuActions =[
+      {
+        title : "View Business Licence Details",
+        url : "sumatra/business-history/:id"
+      }];
   }
 
 }

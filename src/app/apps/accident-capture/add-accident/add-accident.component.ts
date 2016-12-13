2db8b467812af129d42f9ac82889ad53c635c3dc
Joseph Chingalo <profschingalo@gmail.com>
2016-12-13 15:58:01 +0300

    add form for basic information

--- src/app/apps/accident-capture/add-accident/add-accident.component.ts
+++ src/app/apps/accident-capture/add-accident/add-accident.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {AccidentService} from "../../../services/accident.service";
 
 @Component({
   selector: 'app-add-accident',
   templateUrl: './add-accident.component.html',
-  styleUrls: ['./add-accident.component.css']
+  styleUrls: ['./add-accident.component.css'],
+  providers : [AccidentService]
 })
 export class AddAccidentComponent implements OnInit {
 
-  constructor() { }
+  private programStage  :any;
+  constructor(private AccidentService : AccidentService) { }
 
   ngOnInit() {
+    this.AccidentService.getProgram().then(programStage=>{
+      this.programStage = programStage;
+    },error=>{
+      //fail to get programStage
+    });
   }
 
 }

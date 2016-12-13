7e851c1d85a3a6c61cd12f3844885b0f16df70b4
Joseph Chingalo <profschingalo@gmail.com>
2016-12-13 16:39:13 +0300

    update basic information for accident

--- src/app/apps/accident-capture/update-accident/update-accident.component.ts
+++ src/app/apps/accident-capture/update-accident/update-accident.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {AccidentService} from "../../../services/accident.service";
+import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-update-accident',
   templateUrl: './update-accident.component.html',
-  styleUrls: ['./update-accident.component.css']
+  styleUrls: ['./update-accident.component.css'],
+  providers:[AccidentService]
 })
 export class UpdateAccidentComponent implements OnInit {
 
-  constructor() { }
+  private programStage : any;
+  private event : any;
+  private id;
+
+  constructor(private AccidentService : AccidentService,private route: ActivatedRoute,private router: Router) {
+    this.route.params.forEach((params: Params) => {
+      this.id = params['id'];
+    })
+  }
 
   ngOnInit() {
+    this.AccidentService.getProgram().then(programStage=>{
+      this.programStage = programStage;
+      this.AccidentService.get(this.id).then(event=>{
+        this.event = event;
+      },error=>{
+        //fail to get accident
+      });
+    },error=>{
+      //fail to get programStage
+    });
   }
 
 }

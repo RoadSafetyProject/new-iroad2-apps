84f480ac386b50e85937881bbd293ef0e8fd7907
Vincent P. Minde <vincentminde@gmail.com>
2017-01-17 18:03:10 +0300

    Added offence driver and vehicle

--- src/app/apps/offence-capture/view-offence-vehicle/view-offence-vehicle.component.ts
+++ src/app/apps/offence-capture/view-offence-vehicle/view-offence-vehicle.component.ts
@@ -1,22 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Params,Router,NavigationEnd } from '@angular/router';
+import {IEventsWrapper} from "../../../models/event";
+import {IProgramStage} from "../../../models/program-stage";
+import {VehicleService} from "../../../services/vehicle.service";
+import {OffenceService} from "../../../services/offence.service";
 
 @Component({
   selector: 'app-view-offence-vehicle',
   templateUrl: './view-offence-vehicle.component.html',
-  styleUrls: ['./view-offence-vehicle.component.css']
+  styleUrls: ['./view-offence-vehicle.component.css'],
+  providers:[VehicleService,OffenceService]
 })
 export class ViewOffenceVehicleComponent implements OnInit {
 
+  vehicleProgramStage;
+  vehicle
   private id;
 
-  constructor(private route: ActivatedRoute,private router: Router) {
+  constructor(private route: ActivatedRoute,private router: Router,private vehicleService:VehicleService, private offenceService :OffenceService) {
     this.route.params.forEach((params: Params) => {
       this.id = params['id'];
     });
   }
 
   ngOnInit() {
+    this.route.params.forEach((params:Params) => {
+      this.vehicleService.getProgram().then((vehicleProgram) => {
+        this.offenceService.getProgram().then((offenceProgram:IProgramStage) => {
+          this.vehicleProgramStage = vehicleProgram;
+          this.offenceService.get(params['id']).then((offence:any) => {
+            offenceProgram.programStageDataElements.forEach((programStageDataElement)=> {
+              if (programStageDataElement.dataElement.name == "Program_Vehicle") {
+                offence.dataValues.forEach((dataValue) => {
+                  if (dataValue.dataElement == programStageDataElement.dataElement.id) {
+                    this.vehicleService.get(dataValue.value).then((vehicle:any) => {
+                      this.vehicle = vehicle;
+                    })
+                  }
+                })
+              }
+            })
+          })
+        })
+      })
+    });
   }
 
 }
